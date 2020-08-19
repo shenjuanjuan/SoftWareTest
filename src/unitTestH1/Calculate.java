cb93adaf29af3245032d6a6cff8222aa4c88c30e
sjj <1710325451@qq.com>
2020-08-19 16:46:16 +0800

    单元测试

---
+++ src/unitTestH1/Calculate.java
@@ -0,0 +1,21 @@
+package unitTestH1;
+
+public class Calculate {
+	
+	public int add(int x, int y) {
+		return x + y;
+	}
+	
+	public int minus(int x, int y) {
+		return x - y;
+	}
+	
+	public int divide(int x, int y) {
+		return x / y;
+	}
+	
+	public int multi(int x, int y) {
+		return x * y;
+	}
+
+}
